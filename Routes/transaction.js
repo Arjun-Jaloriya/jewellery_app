9d7dc694d387a3d13bd5d1eacd013c4d8c031406
Arjun-jaloriya <arjunjaloriya100@gmail.com>
2023-12-02 19:19:42 +0530

    2

--- Routes/transaction.js
+++ Routes/transaction.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { add_transaction ,get_transaction,update_transaction} = require("../Controllers/transaction");
-// const { issignin } = require("../middleware/authmiddleware");
+const { issignin } = require("../middleware/authmiddleware");
 const transactionrouter = express.Router();
 
 
-transactionrouter.post("/add-transaction",add_transaction);
-transactionrouter.get("/get-transaction/:id",get_transaction);
-transactionrouter.put("/update-transaction/:id",update_transaction);
+transactionrouter.post("/add-transaction",issignin,add_transaction);
+transactionrouter.get("/get-transaction/:id",issignin,get_transaction);
+transactionrouter.put("/update-transaction/:id",issignin,update_transaction);
 
 module.exports = transactionrouter;
